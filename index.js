675e186cf6b86d20a95f25483c8716721ef66160
Kate Popova <popova1ea@gmail.com>
2023-01-01 16:16:59 -0800

    commit tree 522bb661e52f4ce5c6b9f689b44b169098df06d1

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+ // A 4-element Array of a String, String, String, and Number corresponding to a first name, family name, title, and pay rate per hour
+
+function createEmployeeRecord(array) {
+let [firstName, familyName, title, payPerHour, timeInEvents, timeOutEvents] = array;
+let empObj = {firstName, familyName, title, payPerHour, timeInEvents, timeOutEvents};
+empObj.timeInEvents = [];
+empObj.timeOutEvents = [];
+
+   // Returns JavaScript Object with keys:
+//    let  employeeRecord =
+//    {
+//         firstName: [0],
+//         familyName: [1],
+//         title: [2],
+//         payPerHour: [3],
+//         timeInEvents: [],
+//         timeOutEvents: []
+//     }
+//     return employeeRecord;
+return empObj;
+}    
+ console.log(createEmployeeRecord())
+
+ //createEmployeeRecords Argument(s) Array of Arrays =>(arrOfArr)
+
+
+ function createEmployeeRecords(arrOfArr) {
+   return arrOfArr.map(array => createEmployeeRecord(array) )
+    }
+
+function createTimeInEvent (empObj, empStamp){
+
+
+    return employeeRecord;
+}
+
